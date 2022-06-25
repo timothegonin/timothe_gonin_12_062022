ade79b21a22ccf39c629d72e08172a5e945a4a13
TimotheGonin <gonin.timothe@gmail.com>
2022-06-25 15:59:43 +0200

    🚸 responsiv main navbar

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,29 +4,32 @@ import Logo from '../../assets/logo.svg'
 
 function Header() {
   return (
-    <Navbar bg="secondary" className="d-flex justify-content-between">
+    <Navbar bg="secondary" className="d-flex justify-content-between" expand="md" variant="dark">
       <Container className="m-0 px-4" fluid>
         <Navbar.Brand
           href="/"
-          className="col-2 m-0 d-flex align-items-center justify-content-between"
+          className="m-0 d-flex align-items-center"
         >
           <img alt="" src={Logo} />{' '}
-          <span className="text-primary">SportSee</span>
+          <span className="text-primary ms-2">SportSee</span>
         </Navbar.Brand>
-        <Nav className="col-10 d-flex justify-content-around text-primary">
-          <Nav.Link href="/" className="nav-bar__link text-light">
-            Accueil
-          </Nav.Link>
-          <Nav.Link href="/" className="nav-bar__link text-light ">
-            Profil
-          </Nav.Link>
-          <Nav.Link href="/" className="nav-bar__link text-light ">
-            Réglage
-          </Nav.Link>
-          <Nav.Link href="/" className="nav-bar__link text-light ">
-            Communauté
-          </Nav.Link>
-        </Nav>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className='justify-content-center'>
+          <Nav className="w-100 d-flex justify-content-around text-primary">
+            <Nav.Link href="/" className="nav-bar__link text-light">
+              Accueil
+            </Nav.Link>
+            <Nav.Link href="/" className="nav-bar__link text-light ">
+              Profil
+            </Nav.Link>
+            <Nav.Link href="/" className="nav-bar__link text-light ">
+              Réglage
+            </Nav.Link>
+            <Nav.Link href="/" className="nav-bar__link text-light ">
+              Communauté
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
     </Navbar>
   )
