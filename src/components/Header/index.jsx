9b228a642f3228e77d6fc158311363cfd3e3a4e0
TimotheGonin <gonin.timothe@gmail.com>
2022-06-25 11:13:43 +0200

    💄 add header's styles / colors/ layout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,23 +4,26 @@ import Logo from '../../assets/logo.svg'
 
 function Header() {
   return (
-    <Navbar bg="dark" className="d-flex justify-content-between">
-      <Container fluid>
-        <Navbar.Brand href="/" className="col-2">
+    <Navbar bg="secondary" className="d-flex justify-content-between">
+      <Container className="m-0 px-4" fluid>
+        <Navbar.Brand
+          href="/"
+          className="col-2 m-0 d-flex align-items-center justify-content-between"
+        >
           <img alt="" src={Logo} />{' '}
           <span className="text-primary">SportSee</span>
         </Navbar.Brand>
         <Nav className="col-10 d-flex justify-content-around text-primary">
-          <Nav.Link href="/" className="text-light fw-bold">
+          <Nav.Link href="/" className="nav-bar__link text-light">
             Accueil
           </Nav.Link>
-          <Nav.Link href="/" className="text-light fw-bold">
+          <Nav.Link href="/" className="nav-bar__link text-light ">
             Profil
           </Nav.Link>
-          <Nav.Link href="/" className="text-light fw-bold">
+          <Nav.Link href="/" className="nav-bar__link text-light ">
             Réglage
           </Nav.Link>
-          <Nav.Link href="/" className="text-light fw-bold">
+          <Nav.Link href="/" className="nav-bar__link text-light ">
             Communauté
           </Nav.Link>
         </Nav>
