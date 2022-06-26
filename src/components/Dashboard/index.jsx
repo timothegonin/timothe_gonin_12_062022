fbde712cfe36fc4e954baa819497e1995b0b901e
TimotheGonin <gonin.timothe@gmail.com>
2022-06-26 10:20:43 +0200

    ♻️ simplify structure + add sizes

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -5,30 +5,23 @@ function Dashboard() {
     <main>
       <h1>Bonjour Thomas</h1>
       <p>Féliciation ! Vous avez explosé vos objectifs hier 👏</p>
-      <div className="container-fluid">
-        <div className="row">
-          <div className='col-10 bg-danger container'>
-            <div className="row bg-warning pb-1">
-              <div className="p-5 bg-info"></div>
-            </div>
-            <div className="row pt-1 d-flex justify-content-between">
-              <div className="col-3 p-0">
-                <div className="p-3 border bg-info">Custom column padding</div>
-              </div>
-              <div className="col-3 p-0">
-                <div className="p-3 border bg-info">Custom column padding</div>
-              </div>
-              <div className="col-3 p-0">
-                <div className="p-3 border bg-info">Custom column padding</div>
-              </div>
-            </div>
+
+      <div className="d-flex justify-content-evenly">
+        {/* Charts Block */}
+        <div className="charts__containerss">
+          <div className="charts__content bg-danger mb-3"></div>
+          <div className="d-flex justify-content-between">
+            <div className="charts__content--small bg-warning"></div>
+            <div className="charts__content--small bg-warning"></div>
+            <div className="charts__content--small bg-warning"></div>
           </div>
-          <div className='col-2 bg-info d-flex flex-column justify-content-between pe-0'>
-            <div className=" p-3 bg-warning"></div>
-            <div className=" p-3 bg-warning"></div>
-            <div className=" p-3 bg-warning"></div>
-            <div className=" p-3 bg-warning"></div>
-          </div>
+        </div>
+        {/* Charts Info */}
+        <div className="charts-info__container d-flex flex-column justify-content-between">
+          <div className="charts-info__content bg-success"></div>
+          <div className="charts-info__content bg-success"></div>
+          <div className="charts-info__content bg-success"></div>
+          <div className="charts-info__content bg-success"></div>
         </div>
       </div>
     </main>
