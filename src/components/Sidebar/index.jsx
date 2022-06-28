2e606f7d8fe2ea242f955f3d14c955ebed0afea2
TimotheGonin <gonin.timothe@gmail.com>
2022-06-28 09:04:48 +0200

    💄 adding sidebar icons, resizing

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -2,29 +2,34 @@ import React from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Button from 'react-bootstrap/Button'
 
+import YogaIcon from '../../assets/icons/yoga.svg'
+import SwimIcon from '../../assets/icons/swim.svg'
+import BikeIcon from '../../assets/icons/bike.svg'
+import Dumbbell from '../../assets/icons/dumbbell.svg'
+
 function Sidebar() {
   return (
-    <aside className="sidebar col-1 bg-secondary d-flex flex-column align-items-center justify-content-center">
+    <aside className="sidebar col-1 bg-secondary d-flex flex-column align-items-center justify-content-end">
       <nav className="sidebar__nav d-flex">
         <ListGroup className="justify-content-between">
-          <ListGroup.Item className="p-0">
-            <Button className="sidebar__button" variant="light">
-              X
+          <ListGroup.Item className="p-0 rounded">
+            <Button className="sidebar__button rounded" variant="light">
+              <img src={YogaIcon} alt="Yoga icon" />
             </Button>{' '}
           </ListGroup.Item>
-          <ListGroup.Item className="p-0">
-            <Button className="sidebar__button" variant="light">
-              X
+          <ListGroup.Item className="p-0 rounded">
+            <Button className="sidebar__button rounded" variant="light">
+              <img src={SwimIcon} alt="Swin icon" />
             </Button>{' '}
           </ListGroup.Item>
-          <ListGroup.Item className="p-0">
-            <Button className="sidebar__button" variant="light">
-              X
+          <ListGroup.Item className="p-0 rounded">
+            <Button className="sidebar__button rounded" variant="light">
+              <img src={BikeIcon} alt="Bike icon" />
             </Button>{' '}
           </ListGroup.Item>
-          <ListGroup.Item className="p-0">
-            <Button className="sidebar__button" variant="light">
-              X
+          <ListGroup.Item className="p-0 rounded">
+            <Button className="sidebar__button rounded" variant="light">
+              <img src={Dumbbell} alt="Dumbbell icon" />
             </Button>{' '}
           </ListGroup.Item>
         </ListGroup>
