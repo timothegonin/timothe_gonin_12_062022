cffa8c70d061c59a8058fc07020b02ab30a3d7e0
TimotheGonin <gonin.timothe@gmail.com>
2022-06-30 10:34:30 +0200

    💄 🚧 radial info styles

--- src/components/Score/index.jsx
+++ src/components/Score/index.jsx
@@ -1,30 +1,39 @@
 import React from 'react'
-import { RadialBarChart, RadialBar } from 'recharts'
+import { RadialBarChart, RadialBar, ResponsiveContainer } from 'recharts'
 
 const data = [
   {
     name: '18-24',
     uv: 31.47,
-    pv: 2400,
+    pv: 90,
     fill: '#FF0000',
   },
 ]
 
 export default function Score() {
   return (
-    <div className="bg-light">
-      <RadialBarChart
-        width={160}
-        height={160}
-        startAngle={180}
-        endAngle={90}
-        innerRadius={90}
-        outerRadius={55}
-        barSize={10}
-        data={data}
-      >
-        <RadialBar cornerRadius={50} dataKey="uv" />
-      </RadialBarChart>
+    <div className="bg-light charts__content--small">
+      <h3 className="charts__title">Score</h3>
+      <div className="score__container bg-success">
+        <div className="score__info">
+          <p className="score__percent mb-1">12%</p>
+          <p>de votre objectif</p>
+        </div>
+        <ResponsiveContainer width="100%" height="100%">
+          <RadialBarChart
+            width={160}
+            height={160}
+            startAngle={180}
+            endAngle={500}
+            innerRadius={90}
+            outerRadius={75}
+            barSize={10}
+            data={data}
+          >
+            <RadialBar cornerRadius={50} dataKey="uv" />
+          </RadialBarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   )
 }
