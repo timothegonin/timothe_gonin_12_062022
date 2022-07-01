5147d84e4dd7731c91878849c4a3e175106794f2
TimotheGonin <gonin.timothe@gmail.com>
2022-07-01 11:00:41 +0200

    🚧 add lineCharts + remove useless-property

--- src/components/SessionDuration/index.jsx
+++ src/components/SessionDuration/index.jsx
@@ -1,13 +1,81 @@
 import React from 'react'
+import { LineChart, Line, XAxis, Tooltip, ResponsiveContainer } from 'recharts'
 
-import Obj from '../../assets/Objectifs.png'
+const data = [
+  {
+    name: 'L',
+    uv: 4000,
+    pv: 2400,
+    amt: 2400,
+  },
+  {
+    name: 'M',
+    uv: 3000,
+    pv: 1398,
+    amt: 2210,
+  },
+  {
+    name: 'M',
+    uv: 2000,
+    pv: 9800,
+    amt: 2290,
+  },
+  {
+    name: 'J',
+    uv: 2780,
+    pv: 3908,
+    amt: 2000,
+  },
+  {
+    name: 'V',
+    uv: 1890,
+    pv: 4800,
+    amt: 2181,
+  },
+  {
+    name: 'S',
+    uv: 2390,
+    pv: 3800,
+    amt: 2500,
+  },
+  {
+    name: 'D',
+    uv: 3490,
+    pv: 4300,
+    amt: 2100,
+  },
+]
 
-function SessionDuration() {
+export default function SessionDuration() {
   return (
-    <div className="charts__content--small bg-warning">
-      <img src={Obj} alt="Objectifs" />
+    <div className="sessionDuration__container">
+      <ResponsiveContainer width="100%" height="100%">
+        <LineChart
+          width={500}
+          height={300}
+          data={data}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 20,
+            bottom: 5,
+          }}
+        >
+          <XAxis
+            dataKey="name"
+            stroke="white"
+            axisLine={false}
+            tickLine={false}
+          />
+          <Tooltip />
+          <Line
+            type="monotone"
+            dataKey="pv"
+            stroke="#fff"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   )
 }
-
-export default SessionDuration
