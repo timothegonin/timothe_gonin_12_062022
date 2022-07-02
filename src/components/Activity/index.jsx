f4dbbc78d456b7590f3cc907a2024df2ec23b9f8
TimotheGonin <gonin.timothe@gmail.com>
2022-07-02 10:43:21 +0200

    💄 bar styles

--- src/components/Activity/index.jsx
+++ src/components/Activity/index.jsx
@@ -2,56 +2,72 @@ import React from 'react'
 import {
   BarChart,
   Bar,
-  // Cell,
   XAxis,
   YAxis,
   CartesianGrid,
   Tooltip,
-  Legend,
   ResponsiveContainer,
 } from 'recharts'
 
 const data = [
   {
-    name: 'Page A',
+    name: '1',
     uv: 4000,
     pv: 2400,
     amt: 2400,
   },
   {
-    name: 'Page B',
+    name: '2',
     uv: 3000,
     pv: 1398,
     amt: 2210,
   },
   {
-    name: 'Page C',
+    name: '3',
     uv: 2000,
     pv: 9800,
     amt: 2290,
   },
   {
-    name: 'Page D',
+    name: '4',
     uv: 2780,
     pv: 3908,
     amt: 2000,
   },
   {
-    name: 'Page E',
+    name: '5',
     uv: 1890,
     pv: 4800,
     amt: 2181,
   },
   {
-    name: 'Page F',
+    name: '6',
     uv: 2390,
     pv: 3800,
     amt: 2500,
   },
   {
-    name: 'Page G',
+    name: '7',
     uv: 3490,
     pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: '8',
+    uv: 5490,
+    pv: 1300,
+    amt: 2100,
+  },
+  {
+    name: '9',
+    uv: 8490,
+    pv: 4300,
+    amt: 2100,
+  },
+  {
+    name: '10',
+    uv: 6490,
+    pv: 2300,
     amt: 2100,
   },
 ]
@@ -70,14 +86,15 @@ export default function Activity() {
             left: 20,
             bottom: 5,
           }}
+          barSize={7}
+          barGap={8}
         >
-          <CartesianGrid strokeDasharray="3 3" />
+          <CartesianGrid vertical={false} strokeDasharray="2" />
           <XAxis dataKey="name" />
           <YAxis />
           <Tooltip />
-          <Legend />
-          <Bar dataKey="pv" fill="#8884d8" />
-          <Bar dataKey="uv" fill="#82ca9d" />
+          <Bar dataKey="pv" fill="#282D30" radius={[3, 3, 0, 0]} />
+          <Bar dataKey="uv" fill="#E60000" radius={[3, 3, 0, 0]} />
         </BarChart>
       </ResponsiveContainer>
     </div>
