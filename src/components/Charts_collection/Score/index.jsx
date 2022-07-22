513a78a4852128873045f76544c063c9e073016a
TimotheGonin <gonin.timothe@gmail.com>
2022-07-22 09:29:00 +0200

    🔥 ♻️ moving styles to .jsx(Score)

--- src/components/Charts_collection/Score/index.jsx
+++ src/components/Charts_collection/Score/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import styled from 'styled-components'
 import { RadialBarChart, RadialBar, ResponsiveContainer } from 'recharts'
 
 const data = [
@@ -10,15 +10,20 @@ const data = [
   },
 ]
 
+/* 
+  ┌─────────────────────────────────────────────────────────────────────────┐
+  │ JSX                                                                     │
+  └─────────────────────────────────────────────────────────────────────────┘
+ */
 export default function Score() {
   return (
-    <div className="bg-light charts__content--small d-flex flex-column align-items-center">
-      <h3 className="charts__title w-100 px-5 mt-4 mb-1">Score</h3>
-      <div className="score__container">
-        <div className="score__info">
-          <p className="score__percent mb-1">12%</p>
+    <ScoreWrapper>
+      <h3>Score</h3>
+      <ScoreChartContent>
+        <ScoreChartInfo>
+          <p className="score__percent">12%</p>
           <p>de votre objectif</p>
-        </div>
+        </ScoreChartInfo>
         <ResponsiveContainer width="100%" height="100%">
           <RadialBarChart
             width={160}
@@ -33,7 +38,67 @@ export default function Score() {
             <RadialBar cornerRadius={50} dataKey="uv" />
           </RadialBarChart>
         </ResponsiveContainer>
-      </div>
-    </div>
+      </ScoreChartContent>
+    </ScoreWrapper>
   )
 }
+
+/* 
+  ┌─────────────────────────────────────────────────────────────────────────┐
+  │ STYLES                                                                  │
+  └─────────────────────────────────────────────────────────────────────────┘
+ */
+
+const ScoreWrapper = styled.div`
+  background-color: #fbfbfb;
+  border-radius: 5px;
+  height: 263px;
+  width: 258px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  h3 {
+    width: 100%;
+    font-weight: 500;
+    font-size: 15px;
+    padding: 0 48px;
+    margin-top: 24px;
+    margin-bottom: 4px;
+  }
+`
+
+const ScoreChartContent = styled.div`
+  height: 160px;
+  width: 160px;
+  position: relative;
+  display: flex;
+  align-content: center;
+  justify-content: center;
+  border-radius: 5px;
+`
+const ScoreChartInfo = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 20px;
+  width: calc(100% - 20px);
+  height: calc(100% - 20px);
+  position: absolute;
+  border-radius: 50%;
+  top: 50%;
+  transform: translateY(-50%);
+  background-color: white;
+  p {
+    text-align: center;
+    font-weight: 500;
+    font-size: 16px;
+    color: #74798c;
+  }
+  p.score__percent {
+    margin-bottom: 5px;
+    color: black;
+    font-weight: 700;
+    font-size: 26px;
+  }
+`
