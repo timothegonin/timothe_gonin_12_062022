246e3aa342812a665ed24ce00a593101d4eda27a
TimotheGonin <gonin.timothe@gmail.com>
2022-07-26 16:11:51 +0200

    🔥  🐛 useless removing and clean file

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -31,42 +31,27 @@ function Dashboard() {
         </Container>
       </section>
       <section>
-        <ContentWrapper fluid className="bg-success mx-0">
+        <ContentWrapper fluid className="mx-0">
           <Row className="d-flex justify-content-center">
             <ChartsWrapper>
               <Row>
-                <ChartFullSize className="bg-danger">
+                <ChartFullSize>
                   <Activity />
                 </ChartFullSize>
               </Row>
               <Row>
-                <ChartsGroup className="bg-info">
+                <ChartsGroup>
                   <SessionsDuration />
                   <ActivityType />
                   <Score />
                 </ChartsGroup>
               </Row>
             </ChartsWrapper>
-            <CounterWrapper className="bg-warning">
+            <CounterWrapper>
               <Counters />
             </CounterWrapper>
           </Row>
         </ContentWrapper>
-        {/* <Container fluid>
-          <Row>
-            <Col>
-              <Activity />
-            </Col>
-          </Row>
-          <ChartsGroup>
-            <SessionsDuration />
-            <ActivityType />
-            <Score />
-          </ChartsGroup>
-        </Container>
-        <CountersWrapper>
-          <Counters />
-        </CountersWrapper> */}
       </section>
     </MainWrapper>
   )
@@ -87,8 +72,22 @@ const MainWrapper = styled.main`
     max-width: 1126px;
   }
 `
+
 const ContentWrapper = styled(Container)`
   max-width: 1126px;
+`
+
+const ChartsWrapper = styled(Container)`
+  width: 835px;
+  height: 611px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const ChartFullSize = styled(Col)`
+  margin: 0;
+  padding: 0;
 `
 
 const ChartsGroup = styled(Col)`
@@ -98,34 +97,12 @@ const ChartsGroup = styled(Col)`
   padding: 0;
   display: flex;
   justify-content: space-between;
+  @media screen and (min-width: 1406px) {
+    margin-bottom: 0;
+  }
 `
-// const CountersWrapper = styled(Container)`
-//   margin: 10px 0;
-//   display: flex;
-//   justify-content: space-between;
-//   flex-wrap: wrap;
-//   min-height: 280px;
-// `
 
-const ChartsWrapper = styled(Container)`
-  width: 835px;
-  height: 611px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-`
-const ChartFullSize = styled(Col)`
-  margin: 0;
-  padding: 0;
-  /* height: 320px; */
-  /* width: 835px; */
-`
-// const ChartsGroup = styled(Col)`
-//   height: 265px;
-//   /* width: 835px; */
-// `
 const CounterWrapper = styled(Col)`
-  /* margin-left: 30px; */
   padding: 0;
   margin: 10px 0;
   display: flex;
