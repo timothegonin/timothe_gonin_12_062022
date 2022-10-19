63a39dfe202e6d0f7bbc122e84ebf411189a5105
TimotheGonin <gonin.timothe@gmail.com>
2022-10-19 16:04:43 +0200

    🚧 model for main user data

---
+++ src/service/models/User.js
@@ -0,0 +1,32 @@
+import { USER_MAIN_DATA } from '../data-MOCKED'
+
+class User {
+  constructor(data) {
+    this._id = data.id
+    this._userInfos = data.userInfos
+    this._todayScore = data.todayScore
+    this._keyData = data.keyData
+  }
+
+  get id() {
+    return this._id
+  }
+  get firstName() {
+    return this._userInfos.firstName
+  }
+  get todayScore() {
+    return this._todayScore
+  }
+  get caloriesCount() {
+    return this._keyData.caloriesCount
+  }
+  get proteinCount() {
+    return this._keyData.proteinCount
+  }
+  get carbonhydrateCount() {
+    return this._keyData.carbonhydrateCount
+  }
+  get lipidcount() {
+    return this._keyData.lipidCount
+  }
+}
