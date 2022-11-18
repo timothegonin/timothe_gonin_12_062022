95f170661fa05f73080b1f8ed2d9fe07025736fa
TimotheGonin <gonin.timothe@gmail.com>
2022-11-18 10:18:12 +0100

    🎨 🏗️ ErrorMessage comp to Error Page (router)

---
+++ src/components/ErrorMessage/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ErrorMessage = () => {
+  return (
+    <section>
+      <h2>ErrorMessage</h2>
+    </section>
+  )
+}
+
+export default ErrorMessage
--- src/utils/Router/index.jsx
+++ src/utils/Router/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Dashboard from '../../Pages/Dashboard'
-import Error from '../../components/Error'
+import Error from '../../Pages/Error'
 
 const Router = () => {
   return (
