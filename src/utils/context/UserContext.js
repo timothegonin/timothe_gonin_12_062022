ac0608309be7a448911f96e303aa131f6c7b73a2
TimotheGonin <gonin.timothe@gmail.com>
2022-11-25 13:03:31 +0100

    🔥 Remove useless UserActivity model + context

--- src/utils/context/UserContext.js
+++ src/utils/context/UserContext.js
@@ -2,13 +2,7 @@ import { createContext } from 'react'
 import { User } from '../service/models/User'
 import { UserAverageSessions } from '../service/models/UserAverageSessions'
 import { UserActivityType } from '../service/models/UserActivityType'
-import { UserActivity } from '../service/models/UserActivity'
-import {
-  // USER_MAIN_DATA,
-  USER_AVERAGE_SESSIONS,
-  USER_PERFORMANCE,
-  USER_ACTIVITY,
-} from '../service/data-MOCKED'
+import { USER_AVERAGE_SESSIONS, USER_PERFORMANCE } from '../service/data-MOCKED'
 
 export const UserContext = createContext()
 
@@ -16,12 +10,9 @@ const UserContextProvider = (props) => {
   const user = new User(12)
   const averageSessions = new UserAverageSessions(USER_AVERAGE_SESSIONS[0])
   const activityType = new UserActivityType(USER_PERFORMANCE[0])
-  const activity = new UserActivity(USER_ACTIVITY[0])
 
   return (
-    <UserContext.Provider
-      value={{ user, averageSessions, activityType, activity }}
-    >
+    <UserContext.Provider value={{ user, averageSessions, activityType }}>
       {props.children}
     </UserContext.Provider>
   )
