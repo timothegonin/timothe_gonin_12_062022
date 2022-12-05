531a1ea14a68d25f319e9871c7898e4f15463a91
TimotheGonin <gonin.timothe@gmail.com>
2022-12-05 13:39:49 +0100

    🏷️♻️ SessionDuration chart PropTypes

--- src/components/Charts_collection/SessionsDuration/index.jsx
+++ src/components/Charts_collection/SessionsDuration/index.jsx
@@ -78,7 +78,12 @@ export default SessionsDuration
   └─────────────────────────────────────────────────────────────────────────┘
  */
 SessionsDuration.propTypes = {
-  sessionsDuration: PropTypes.arrayOf(PropTypes.object),
+  sessionsDuration: PropTypes.arrayOf(
+    PropTypes.shape({
+      day: PropTypes.string,
+      sessionLength: PropTypes.number,
+    })
+  ),
 }
 
 /* 
