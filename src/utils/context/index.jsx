19b0c7de7540b7a697901c2c05525654a7781e36
TimotheGonin <gonin.timothe@gmail.com>
2023-01-19 14:48:07 +0100

    🚧 User Data to local storage

--- src/utils/context/index.jsx
+++ src/utils/context/index.jsx
@@ -1,19 +1,21 @@
 import { createContext, useEffect, useReducer, useState } from 'react'
 import { userReducer, initialState } from '../reducer/'
-// import { User } from '../service/models/User'
+import { User } from '../service/models/User'
 
 export const UserContext = createContext()
 
 const UserContextProvider = (props) => {
   const [state, dispatch] = useReducer(userReducer, initialState)
   const userID = state.id
-  const [items, setItem] = useState()
+  const [user, setUser] = useState()
 
   useEffect(() => {
     localStorage.setItem('userID', JSON.stringify(userID))
     const items = JSON.parse(localStorage.getItem('userID'))
     if (items) {
       console.log(items, 'LOCAL')
+      setUser(new User(Number(items)))
+      localStorage.setItem('userDATA', JSON.stringify(user))
     }
   }, [userID])
 
