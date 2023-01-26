f7c2d99e6d1b483adc1edce6b78055a2cb1a76d4
TimotheGonin <gonin.timothe@gmail.com>
2023-01-26 16:45:36 +0100

    🚧 multiple urls fetch

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -14,20 +14,35 @@ function Dashboard() {
   const { dispatch } = useContext(UserContext)
   const [userDataFromFetch, setUserDataFromFetch] = useState({})
   const [isDataLoading, setDataLoading] = useState(false)
+  const urls = [
+    `http://localhost:3000/user/${idFromURL}`,
+    `http://localhost:3000/user/${idFromURL}/activity`,
+    `http://localhost:3000/user/${idFromURL}/average-sessions`,
+    `http://localhost:3000/user/${idFromURL}/performance`,
+  ]
 
   useEffect(() => {
     async function fetchUserData() {
-      setDataLoading(true)
-      try {
-        const response = await fetch(`http://localhost:3000/user/${idFromURL}`)
-        const { data } = await response.json()
-        setUserDataFromFetch(data)
-      } catch (err) {
-        console.log(err)
-        // setError(true)
-      } finally {
-        setDataLoading(false)
-      }
+      // setDataLoading(true)
+      // try {
+      //   const response = await fetch(
+      //     `http://localhost:3000/user/${idFromURL}/activity`
+      //   )
+      //   const { data } = await response.json()
+      //   setUserDataFromFetch(data)
+      // } catch (err) {
+      //   console.log(err)
+      //   // setError(true)
+      // } finally {
+      //   setDataLoading(false)
+      // }
+      const arrayOfResponses = await Promise.all(
+        urls.map((url) =>
+          fetch(url)
+            .then((res) => res.json())
+            .then(({ data }) => console.log(data))
+        )
+      )
     }
     fetchUserData()
 
