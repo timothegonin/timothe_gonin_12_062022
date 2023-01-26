84ce74d5c85e930c33370c37ef3f6a807c88e1a7
TimotheGonin <gonin.timothe@gmail.com>
2023-01-26 16:09:39 +0100

    🚧 first step of fetch (user main data)

--- src/Pages/Dashboard/index.jsx
+++ src/Pages/Dashboard/index.jsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../utils/context'
 import { useParams } from 'react-router-dom'
 import Analytics from '../../Containers/Analytics'
@@ -12,8 +12,25 @@ import { User } from '../../utils/service/models/User'
 function Dashboard() {
   const idFromURL = useParams().id
   const { dispatch } = useContext(UserContext)
+  const [userDataFromFetch, setUserDataFromFetch] = useState({})
+  const [isDataLoading, setDataLoading] = useState(false)
 
   useEffect(() => {
+    async function fetchUserData() {
+      setDataLoading(true)
+      try {
+        const response = await fetch(`http://localhost:3000/user/${idFromURL}`)
+        const { data } = await response.json()
+        setUserDataFromFetch(data)
+      } catch (err) {
+        console.log(err)
+        // setError(true)
+      } finally {
+        setDataLoading(false)
+      }
+    }
+    fetchUserData()
+
     const setUser = (entry) => {
       dispatch({
         type: 'SET_NEW_USER',
@@ -24,8 +41,9 @@ function Dashboard() {
     }
     setUser(idFromURL)
   }, [idFromURL])
+  console.log(userDataFromFetch)
 
-  return <Analytics />
+  return isDataLoading ? <p>Chargement</p> : <Analytics />
 }
 
 export default Dashboard
