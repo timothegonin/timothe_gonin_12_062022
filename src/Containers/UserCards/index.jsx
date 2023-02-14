c3a1a53040553f671adc42babea59ce92fc52d0e
TimotheGonin <gonin.timothe@gmail.com>
2023-02-14 11:37:33 +0100

    💄 userCards layout

--- src/Containers/UserCards/index.jsx
+++ src/Containers/UserCards/index.jsx
@@ -1,12 +1,18 @@
 import UserCard from '../../components/UserCard'
+
+/* 
+  ┌─────────────────────────────────────────────────────────────────────────┐
+  │ JSX                                                                     │
+  └─────────────────────────────────────────────────────────────────────────┘
+ */
 const UserCards = () => {
   const ids = [12, 18]
   return (
-    <div>
+    <section className="mt-5 d-flex justify-content-evenly">
       {ids.map((id, index) => (
         <UserCard id={id} key={`userCard ${index}`} />
       ))}
-    </div>
+    </section>
   )
 }
 
