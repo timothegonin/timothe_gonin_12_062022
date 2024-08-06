f0737f842cd805347d01ffbbea355b776a105653
timotheG <gonin.timothe@gmail.com>
2024-08-06 18:23:54 +0200

    ✅ test: header's links in DOM

--- src/App.test.js
+++ src/App.test.js
@@ -12,3 +12,20 @@ test('Should render the brand logo', () => {
   const brandLogo = screen.getByAltText("SportSee's Logo")
   expect(brandLogo).toBeInTheDocument()
 })
+test('Should render the Header component', () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  )
+  const homeLink = screen.getByRole('link', { name: 'Accueil' })
+  const disabledProfileLink = screen.getByRole('button', { name: 'Profil' })
+  const disabledSettingLink = screen.getByRole('button', { name: 'Réglage' })
+  const disabledCommunityLink = screen.getByRole('button', {
+    name: 'Communauté',
+  })
+  expect(homeLink).toBeInTheDocument()
+  expect(disabledProfileLink).toBeInTheDocument()
+  expect(disabledSettingLink).toBeInTheDocument()
+  expect(disabledCommunityLink).toBeInTheDocument()
+})
