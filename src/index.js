953cc4977b0587c44c9191f9daf8d5d43dbc4839
timotheG <gonin.timothe@gmail.com>
2024-08-14 17:19:14 +0200

    🔧 disabling bootstrap + colors config

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import './utils/styles/scss/styles.scss'
+// import './utils/styles/scss/styles.scss'
 import GlobalStyles from './utils/styles/GlobalStyles'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ff0000',
+        secondary: '#020203',
+      },
+    },
   },
   plugins: [],
 }
