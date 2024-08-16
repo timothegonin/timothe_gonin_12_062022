1a87ddc60cf4c0e64f860b5712e47597c93593ae
timotheG <gonin.timothe@gmail.com>
2024-08-16 15:02:40 +0200

    💄 custom toast primary styles

--- src/components/DataInfo/index.jsx
+++ src/components/DataInfo/index.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react'
-import { ToastContainer } from 'react-bootstrap'
-import Toast from 'react-bootstrap/Toast'
 
 /**
  * Component that displays a Toast with information about the data source (API or mock data).
@@ -10,29 +8,40 @@ import Toast from 'react-bootstrap/Toast'
 
 function DataInfo() {
   //Toast handler
-  const [toastDisplayed, setToastDisplayed] = useState(true)
-  const handleToast = () => setToastDisplayed(!toastDisplayed)
+  // const [toastDisplayed, setToastDisplayed] = useState(true)
+  // const handleToast = () => setToastDisplayed(!toastDisplayed)
 
   const dataMode = process.env.REACT_APP_DATA_MODE
   const dataModeMessage =
     dataMode === 'API' ? `API` : `mock des données de l'API`
 
   return (
-    <ToastContainer
-      className="p-5 mt-5"
-      position="top-end"
-      style={{ zIndex: '999' }}
+    <div
+      role="alert"
+      aria-live="assertive"
+      className="inline-flex flex-col border border-slate-200 rounded-lg"
     >
-      <Toast show={toastDisplayed} onClose={handleToast}>
-        <Toast.Header closeButton={true}>
-          <strong className="me-auto">DATA MODE</strong>
-          <small>📌</small>
-        </Toast.Header>
-        <Toast.Body>
-          Les données proviennent de : {`${dataModeMessage}`} .
-        </Toast.Body>
-      </Toast>
-    </ToastContainer>
+      <div className="flex justify-between px-5 py-1 bg-slate-200">
+        <strong className="font-semibold">
+          <span>🎯</span> DATA MODE
+        </strong>
+        <button className="hover:fill-red-500 hover:stroke-red-500">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            fill="currentColor"
+            className="bi bi-x-l"
+            viewBox="0 0 16 16"
+          >
+            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z" />
+          </svg>
+        </button>
+      </div>
+      <p className="px-5 py-2">
+        Les données proviennent de : {`${dataModeMessage}`}
+      </p>
+    </div>
   )
 }
 
