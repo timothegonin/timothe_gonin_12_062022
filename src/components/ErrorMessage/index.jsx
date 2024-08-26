1a954750635efe6bed6d1dfbca53e9b209692146
timotheG <gonin.timothe@gmail.com>
2024-08-26 19:46:40 +0200

    🎨 improving sementics

--- src/components/ErrorMessage/index.jsx
+++ src/components/ErrorMessage/index.jsx
@@ -20,8 +20,11 @@ const ErrorMessage = () => {
     : `La page demandée n'éxiste pas.`
 
   return (
-    <ContentWrapper className="flex flex-col justify-center items-center gap-5">
-      <h2 className="font-bold text-2xl text-primary">{message}</h2>
+    <ContentWrapper className="flex flex-col justify-center items-center gap-10">
+      <div className="flex flex-col items-center">
+        <h2 className="font-bold text-4xl text-primary inline-flex">404</h2>
+        <p className="font-bold text-2xl text-secondary">{message}</p>
+      </div>
       <Link
         to="/"
         className="text-white bg-primary p-2 rounded hover:bg-red-700 transition duration-150 ease-in-out delay-50"
