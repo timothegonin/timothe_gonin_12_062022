85b61e1fc375e592906da59ea0d751a67e383fc3
timotheG <gonin.timothe@gmail.com>
2024-08-28 11:41:54 +0200

    ✨ open/close dropdown

--- src/Containers/Header/index.jsx
+++ src/Containers/Header/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import Logo from '../../assets/logo.svg'
 
@@ -13,6 +14,7 @@ import Logo from '../../assets/logo.svg'
   └─────────────────────────────────────────────────────────────────────────┘
  */
 function Header() {
+  const [isOpen, setIsOpen] = useState(false)
   return (
     <header className="bg-secondary flex flex-row items-center justify-between  p-3 sm:py-4 sm:px-9 sticky top-0 z-10">
       <div className="flex justify-center items-center">
@@ -20,17 +22,28 @@ function Header() {
         <h1 className="text-red-500 font-medium ms-4 text-2xl">SportSee</h1>
       </div>
       <nav className="relative flex justify-end w-2/6">
-        <button className=" text-slate-50 font-medium text-xl p-2">Menu</button>
-        <div className="absolute right-0 bg-secondary top-full w-full rounded-b-md">
-          <ul className="flex flex-col items-end p-3 gap-2">
-            <Link to="/">
-              <li className="text-slate-100 font-medium text-l">Accueil</li>
-            </Link>
-            <li className="text-slate-100 font-medium text-l">Profil</li>
-            <li className="text-slate-100 font-medium text-l">Réglage</li>
-            <li className="text-slate-100 font-medium text-l">Communauté</li>
-          </ul>
-        </div>
+        <button
+          className="text-slate-50 font-medium text-xl p-2"
+          onClick={() => {
+            setIsOpen(!isOpen)
+          }}
+        >
+          Menu
+        </button>
+        {isOpen && (
+          <div className="absolute right-0 bg-secondary top-full w-full rounded-b-md">
+            <ul className="flex flex-col items-end p-3 gap-2">
+              <Link to="/">
+                <li className="text-slate-100 font-medium text-l hover:text-primary">
+                  Accueil
+                </li>
+              </Link>
+              <li className="text-slate-100 font-medium text-l">Profil</li>
+              <li className="text-slate-100 font-medium text-l">Réglage</li>
+              <li className="text-slate-100 font-medium text-l">Communauté</li>
+            </ul>
+          </div>
+        )}
       </nav>
       {/* <nav className="flex w-3/4 pe-14">
         <ul className="flex flex-row grow justify-between">
